2e2b415252e55628d46528482617dd86f47fa4be
Matias Niemelä <matias@yearofmoo.com>
2012-10-08 18:52:25 -0400

    code added

---
+++ src/SafeApply.js
@@ -0,0 +1,14 @@
+angular.module('SafeApply', []).run(function($rootScope) {
+
+  $rootScope.$safeApply = function($scope, fn, force) {
+    $scope = $scope || $rootScope;
+    fn = fn || function() {};
+    if(force || !$scope.$$phase) {
+      $scope.apply(fn);
+    }
+    else {
+      fn();
+    }
+  };
+
+});
